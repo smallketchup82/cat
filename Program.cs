738c6739cd453192575c12a0ad5c6d8b1cd0ff87
smallketchup82 <69545310+smallketchup82@users.noreply.github.com>
2022-11-22 00:41:24 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+
+
+var client = new HttpClient();
+
+void downloadCat()
+{
+    Console.WriteLine("Obtaining cat image...");
+
+    var image = client.GetStreamAsync("https://cataas.com/cat/cute");
+
+    var idk = new Random()
+
+    var file = new FileStream($"{"".Join(idk.Next()}");
+    Console.WriteLine(image);
+}
+
+downloadCat();
