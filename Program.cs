e6b6133ce5de1f73f21d86fd757683b3d8db443b
smallketchup82 <69545310+smallketchup82@users.noreply.github.com>
2022-11-23 08:48:30 -0500

    ITS FUN DOE

--- Program.cs
+++ Program.cs
@@ -1,18 +1,142 @@
 ﻿using System.Net;
+using System.Reflection;
+using NAudio;
+using NAudio.Wave;
 
-
-var client = new HttpClient();
-
-void downloadCat()
+public class LoopStream : WaveStream
 {
-    Console.WriteLine("Obtaining cat image...");
+    WaveStream sourceStream;
 
-    var image = client.GetStreamAsync("https://cataas.com/cat/cute");
+    /// <summary>
+    /// Creates a new Loop stream
+    /// </summary>
+    /// <param name="sourceStream">The stream to read from. Note: the Read method of this stream should return 0 when it reaches the end
+    /// or else we will not loop to the start again.</param>
+    public LoopStream(WaveStream sourceStream)
+    {
+        this.sourceStream = sourceStream;
+        this.EnableLooping = true;
+    }
 
-    var idk = new Random()
+    /// <summary>
+    /// Use this to turn looping on or off
+    /// </summary>
+    public bool EnableLooping { get; set; }
 
-    var file = new FileStream($"{"".Join(idk.Next()}");
-    Console.WriteLine(image);
+    /// <summary>
+    /// Return source stream's wave format
+    /// </summary>
+    public override WaveFormat WaveFormat
+    {
+        get { return sourceStream.WaveFormat; }
+    }
+
+    /// <summary>
+    /// LoopStream simply returns
+    /// </summary>
+    public override long Length
+    {
+        get { return sourceStream.Length; }
+    }
+
+    /// <summary>
+    /// LoopStream simply passes on positioning to source stream
+    /// </summary>
+    public override long Position
+    {
+        get { return sourceStream.Position; }
+        set { sourceStream.Position = value; }
+    }
+
+    public override int Read(byte[] buffer, int offset, int count)
+    {
+        int totalBytesRead = 0;
+
+        while (totalBytesRead < count)
+        {
+            int bytesRead = sourceStream.Read(buffer, offset + totalBytesRead, count - totalBytesRead);
+            if (bytesRead == 0)
+            {
+                if (sourceStream.Position == 0 || !EnableLooping)
+                {
+                    // something wrong with the source stream
+                    break;
+                }
+                // loop
+                sourceStream.Position = 0;
+            }
+            totalBytesRead += bytesRead;
+        }
+        return totalBytesRead;
+    }
 }
 
-downloadCat();
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Mp3FileReader? reader = null;
+
+        try
+        {
+            reader = new Mp3FileReader(Assembly.GetEntryAssembly()!.GetManifestResourceStream("cat.cat.mp3"));
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            reader = new Mp3FileReader("cat.mp3");
+        }
+
+        LoopStream loopthingy = new LoopStream(reader);
+        var waveout = new WaveOutEvent();
+        waveout.Init(loopthingy);
+        waveout.Play();
+
+        while (true) {
+            downloadCat().Wait();
+        }
+    }
+
+    static async Task downloadCat()
+    {
+        Console.WriteLine("Obtaining cat image...");
+
+        HttpClient client = new HttpClient();
+        var image = await client.GetAsync("https://cataas.com/cat/cute");
+        image.EnsureSuccessStatusCode();
+
+        Random res = new Random();
+        string str = "abcdefghijklmnopqrstuvwxyz".ToUpper();
+        int size = 5;
+
+        // Initializing the empty string
+        string ran = "";
+
+        for (int i = 0; i < size; i++)
+        {
+
+            // Selecting a index randomly
+            int x = res.Next(26);
+
+            // Appending the character at the 
+            // index to the random string.
+            ran = ran + str[x];
+        }
+
+        var filename = $"cat{ran}.png";
+
+        var fs = new FileStream(filename, FileMode.CreateNew);
+        await image.Content.CopyToAsync(fs);
+
+        var thing = new System.Diagnostics.Process();
+
+        thing.StartInfo.UseShellExecute = true;
+        thing.StartInfo.FileName = filename;
+        thing.Start();
+        await thing.WaitForExitAsync();
+
+        Thread.Sleep(1000);
+        File.Delete(filename);
+        Console.WriteLine("Cat image saved!");
+    }
+}
