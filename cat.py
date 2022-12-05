35b343574e33e699b5b8c867f217e81127b30ffa
smallketchup82 <69545310+smallketchup82@users.noreply.github.com>
2022-12-05 10:23:10 -0500

    fix spacing & add docs

--- cat.py
+++ cat.py
@@ -6,13 +6,14 @@ import subprocess
 import threading
 import time
 import signal
-
 import pip._vendor.requests as requests
 import playsound
 
+# Kill the program if a fatal error is encountered
 def killProgram():
     os.kill(os.getpid(), signal.SIGKILL)
 
+# Play the song in the background, also loop it
 def playCatSound():
     print("Playing cat song!")
     try:
@@ -23,21 +24,27 @@ def playCatSound():
         killProgram()
     time.sleep(1)
 
+# Start playing the music
 threading.Thread(target=playCatSound, daemon=True).start()
 
+# Main function for downloading the cat
 def downloadCat():
     print("Obtaining cat image...")
     
+    # Gets the cat image from an API
     image = requests.get("https://cataas.com/cat/cute")
     
+    # If the API is down, use a backup API
     if image.status_code != 200:
         print("Using backup API!")
         image = requests.get(requests.get("https://api.thecatapi.com/v1/images/search").json()[0]['url'])
 
+    # Write the image to disk
     filename = f"cat{''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(5))}.png"
     with open(filename, 'wb') as outfile:
         outfile.write(image.content)
 
+    # Function for opening the image, only works on MacOS and Windows since open commands on linux vary by distro
     def openNsomething():
         if platform.system() == "Darwin":
             subprocess.call(('open', '-Wg', filename))
@@ -50,8 +57,8 @@ def downloadCat():
             
         os.remove(filename)
         
+    # Open the image in a new thread, so that the main thread can start loading the next image by the time the current one is opened.
     threading.Thread(target=openNsomething).start()
-
     print("Downloaded cat!")
 
 while True:
